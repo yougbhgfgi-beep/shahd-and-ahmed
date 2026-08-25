2ee3d55587d1e727a3ce03f29ec24a2879c9fcf3
yougbhgfgi@gmail-beep <yougbhgfgi@gmail.com>
2026-08-25 21:28:54 +0300

    New sw2.js to force-clear old broken service worker cache

--- sw.js
+++ sw.js
@@ -1,14 +1,15 @@
-const CACHE_NAME = 'love-v7';
-
+// Old SW - self destruct
 self.addEventListener('install', () => self.skipWaiting());
-
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => {
+      return self.clients.matchAll().then(clients => {
+        clients.forEach(client => client.postMessage('sw-updated'));
+      });
+    })
   );
   self.clients.claim();
 });
-
 self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+  event.respondWith(fetch(event.request));
 });
---
+++ sw2.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'love-v8';
+
+self.addEventListener('install', () => self.skipWaiting());
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
